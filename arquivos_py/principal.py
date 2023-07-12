2dd7cd826394e015b6f84103ca46effc281818c2
LEONARDO RIBEIRO PONTES <leonardo.pontes@alunos.sc.senac.br>
2023-07-12 19:44:59 -0300

    Atualização de arquivos de validação

---
+++ arquivos_py/principal.py
@@ -0,0 +1,65 @@
+import sys
+from PySide6.QtWidgets import QMainWindow,QApplication
+
+from interfaces.cadastroPi import *
+from interfaces.inteface_login_encontreme import *
+from interfaces.projetoDevolucaoObjeto import *
+from interfaces.projetoCadastroObjeto import *
+from interfaces.projetoMenu import *
+
+class Logar(QMainWindow,Ui_Login):
+    def __init__(self) -> None:
+        super(Logar,self).__init__()
+        self.setupUi(self)
+
+        self.btn_logar.clicked.connect(self.logar)
+    
+    def logar(self):
+        senha=self.pegar_senha()
+        user=self.pegar_email()
+        if user=="Admin" and senha=="1234":
+            self.w=Menu()
+            self.w.show()
+            self.close()
+        else:
+            print("senha ou usuario invalio")
+
+class Menu(QMainWindow,Ui_Menu):
+    def __init__(self) -> None:
+        super(Menu,self).__init__()
+        self.setupUi(self)
+        self.btn_cadastro.clicked.connect(self.cadastrarObjeto)
+        self.btn_devolucao.clicked.connect(self.devolucao)
+
+    def cadastrarObjeto(self):
+            self.o=CadastrarObejto()
+            self.o.show()
+
+    def devolucao(self):
+            self.o=Devolucao()
+            self.o.show()
+
+class CadastrarObejto(QMainWindow,Ui_CadastroObjeto):
+    def __init__(self) -> None:
+        super(CadastrarObejto,self).__init__()
+        self.setupUi(self)
+
+class CadastroUsuario(QMainWindow,Ui_CadastroUsuario):
+    def __init__(self) -> None:
+        super(CadastroUsuario,self).__init__()
+        self.setupUi(self)
+
+class Devolucao(QMainWindow,Ui_DevolucaoObjeto):
+    def __init__(self) -> None:
+        super(Devolucao,self).__init__()
+        self.setupUi(self)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    logar = Logar()
+    logar.show()
+    sys.exit(app.exec())
+
+
+
+
